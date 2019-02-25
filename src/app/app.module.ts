51687dbc3b22a23cd241955933b369a633b2361e
Huser <Huser@LTNJ-5MYTH72.corp.hexaware.com>
2019-02-25 13:13:22 -0500

    built all the routes from the dashboard to each page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StatusComponent } from './status/status.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { PersonalComponent } from './personal/personal.component';
+import { ManagerComponent } from './manager/manager.component';
+import { ApplyComponent } from './apply/apply.component';
+import { DecisionComponent } from './decision/decision.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatusComponent,
+    DashboardComponent,
+    LoginComponent,
+    PersonalComponent,
+    ManagerComponent,
+    ApplyComponent,
+    DecisionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  
+}
