8c009d91a8d934ec8c21d96f20f07a052e95b625
tuananvu <hongantcnguyen@yahoo.com>
2019-02-26 18:09:55 -0500

    subscribe to Manager Detail service, also use Router with empId param

--- src/app/manager/manager.component.ts
+++ src/app/manager/manager.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ManagerDetailService } from '../manager-detail.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-manager',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManagerComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  empId:number;
+  manager:any;
+  constructor(private managerDetailService:ManagerDetailService, private route:ActivatedRoute, private router:Router) {
+    //this.empId = 334;
   }
 
+  ngOnInit() {
+    this.getMangerByEmpId();
+  }
+
+  getMangerByEmpId():void{
+    
+    this.managerDetailService.getManagerById(this.route.snapshot.params['empId']).subscribe((data:{}) => {
+      console.log(data);
+      this.manager = data;
+    } )
+  }
 }
