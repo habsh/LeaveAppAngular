1404be4872e29884308bae13ad2950918b129b8a
Huser <Huser@LTNJ-5MYTH72.corp.hexaware.com>
2019-02-27 14:56:53 -0500

    Added UI responsive to adapt to mobile devices and small screens

--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -4,16 +4,28 @@ import { CredentialsService } from '../credentials.service';
 import { HttpClient, HttpHeaders,HttpErrorResponse, HttpHandler } from '@angular/common/http';
 
 var jsontxt=`[{
-  "leaveId": 1234,
+  "leaveId": 123,
   "days": 5,
-  "start": "2012 - 04 - 23 T18: 25: 43.511 Z",
-  "end": "2012 - 04 - 23 T18: 25: 43.511 Z",
+  "start": "2012 - 04 - 23",
+  "end": "2012 - 04 - 28",
   "type": "earned",
   "status": "Pending",
   "reason": "leave",
-  "applied": "2012 - 04 - 23 T18: 25: 43.511 Z",
+  "applied": "2012 - 04 - 20",
   "comments": "commented"
-}]
+},
+{
+  "leaveId": 456,
+  "days": 2,
+  "start": "2012 - 05 - 10",
+  "end": "2012 - 05 - 12",
+  "type": "earned",
+  "status": "Pending",
+  "reason": "leave",
+  "applied": "2012 - 04 - 23",
+  "comments": "commented"
+}
+]
 `;
 var json = JSON.parse(jsontxt);
 
@@ -27,7 +39,8 @@ export class StatusComponent implements OnInit {
   title = 'chkleave';
   leaveData:string[];
   outputData;
-  
+  public show:boolean = true;
+  hideView=-1;
   constructor(private http:HttpClient){
   }
   
@@ -35,8 +48,12 @@ export class StatusComponent implements OnInit {
     this.leaveData=json;
     console.log(this.leaveData);
     this.http.get("https://reqres.in/api/users?page=2").subscribe(data => console.log(data));
-  }
+    }
   create(){
     location.href= "/apply"
+  }
+  expDetails(leav){
+    console.log(leav)
+    leav.showButton = !leav.showButton;
   }
 }
