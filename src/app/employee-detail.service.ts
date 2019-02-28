bf0b25d026f9b9bf76396389f015bbc438d61d40
abudri <17362519+abudri@users.noreply.github.com>
2019-02-28 16:17:53 -0500

    Changes to styling and hardcoded url IP

--- src/app/employee-detail.service.ts
+++ src/app/employee-detail.service.ts
@@ -1,13 +1,10 @@
-// 2/26/2019, Abdullah, based on An's employee-detail.service.ts
-/// ran in cli this to great `employee_detail` service:  ng generate service employee_detail
-
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import {map, catchError, tap} from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
 
 
-const endpoint = 'http://192.168.20.203:8181/leaveapp/';
+const endpoint = 'http://192.168.20.211:8181/leaveapp/';
 const httpOptions ={
     headers: new HttpHeaders({
       'Content-Type':  'application/json'
@@ -27,7 +24,7 @@ export class EmployeeDetailService {
   }
   
   getEmployeeById(empId:number):Observable<any>{
-    //var url='http://192.168.20.203:8181/leaveapp/getEmployee/'+empId;
+    // var url='http://192.168.20.211:8181/leaveapp/getEmployee/'+empId;
     return this.http.get(endpoint + 'getEmployee/' + empId).pipe(map(this.extractData));
     //return this.http.get(url).pipe(map((response:any) => response.JSON()));
   }
