4c349f2f0f7a7466ee199f22d8d9896e35acde00
Christopher Lim <chrisingerlim@gmail.com>
2019-03-04 12:57:01 -0500

    adding http headers to define content type as json

--- src/app/apply/rest.service.ts
+++ src/app/apply/rest.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormGroup } from '@angular/forms';
 import {Observable} from 'rxjs'
+import { HttpHeaders } from '@angular/common/http';
 import { from } from 'rxjs';
 @Injectable({
   providedIn: 'root'
@@ -10,21 +11,26 @@ export class RestService {
 
   private actionUrl: string;
   constructor(private http: HttpClient) { 
-    this.actionUrl = "http://localhost:9999/applyLeave";
+    this.actionUrl = "http://localhost:8765/applyLeave";
   }
 
   public update<T>(formControl: FormGroup): Observable<T>{
-    return this.http.post<T>(this.actionUrl,
-      {
-        startDate : formControl.controls['Start_Date'].value,
-        endDate:formControl.controls['End_Date'].value,
-        numDays:formControl.controls['Number_Of_Days'].value,
-        leaveType:formControl.controls['Leave_Type'].value,
-        reasons:formControl.controls['Leave_Reason'].value,
-        employee:formControl.controls['Employee_ID'].value
 
-      }
-      
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    });
+
+    return this.http.post<T>(this.actionUrl,JSON.stringify({
+
+        "startDate" : formControl.controls['Start_Date'].value,
+        "endDate":formControl.controls['End_Date'].value,
+        "numDays":formControl.controls['Number_Of_Days'].value,
+        "leaveType":formControl.controls['Leave_Type'].value,
+        "reasons":formControl.controls['Leave_Reason'].value,
+        "employee":formControl.controls['Employee_ID'].value
+
+      }),{headers: headers}
     );
   }
 }
